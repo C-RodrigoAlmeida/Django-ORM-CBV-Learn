3e579de7eaadcc121301583eece7997b98f98c79
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-07-04 07:22:01 -0300

    fixing admin imports, now admin pages show tables that are in db correctly

--- src/user/admin/admin.py
+++ src/user/admin/admin.py
@@ -1,6 +1,8 @@
+# src/user/admin.py
 from django.contrib import admin
 from src.user.models.custom_user import CustomUser
-from src.user.models.custom_user import Role
+from src.user.models.role import Role
+
 
 admin.site.register(CustomUser)
 admin.site.register(Role)
