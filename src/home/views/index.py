158a260280c022764eec132446ca1d8f994c0f09
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-07-14 15:24:04 -0300

    Removing content injection to the index of the home app

--- src/home/views/index.py
+++ src/home/views/index.py
@@ -5,8 +5,3 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "index.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["injectme"] = "BASIC INJECTION"
-        return context
