34dc26898e8a335bd9873f27675482480ac9891c
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-07-15 07:04:25 -0300

    Adding default inserts into Role table through migrations

--- src/user/migrations/0006_default_role_inserts.py
+++ src/user/migrations/0006_default_role_inserts.py
@@ -4,11 +4,11 @@ from django.db import migrations
 class Migration(migrations.Migration):
     def create_roles(apps, schema_editor):
         Role = apps.get_model('user', 'Role')
-        role_list = ['Principal', 'Teacher', 'Student']
+        role_list = ['Staff', 'Principal', 'Teacher', 'Student']
         for role in role_list: Role.objects.create(name=role)
 
     dependencies = [
-        ('user', '0005_remove_customuser_name'),        
+        ('user', '0005_remove_customuser_name'),
     ]
 
     operations = [
