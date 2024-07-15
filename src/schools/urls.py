e465c0e12c3ffcfa73a72107740d9a68ef818253
Rodrigo Almeida <c.almeidarodrigo@gmail.com>
2024-07-15 09:21:11 -0300

    Started to setup permission to view, edit and create new schools. Next step is to create the migration necessary to make it all work

--- src/schools/urls.py
+++ src/schools/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from src.schools.views.details import SchoolsDetailsView
 from src.schools.views.list import SchoolsListView
+from src.schools.views.create import SchoolsCreateView
 
 app_name = "schools"
 
 urlpatterns = [
     path("", SchoolsListView.as_view(), name="list"),
+    path('create/', SchoolsCreateView.as_view(), name="create"),
     path("<uuid:id>/", SchoolsDetailsView.as_view(), name="details"),
 ]
--- src/schools/views/create.py
+++ src/schools/views/create.py
@@ -1,6 +1,7 @@
 from django.views.generic import CreateView
 from src.schools.models.school import School
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
-class Create(CreateView):
+class SchoolsCreateView(CreateView, LoginRequiredMixin, PermissionRequiredMixin):
     template_name = 'create.html'
     model = School
